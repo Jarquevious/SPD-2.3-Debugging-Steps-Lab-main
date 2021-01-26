b91b2639a6b0d19e2002a3cb26b34e75867ced7c
Jarquevious <jarquevious@gmail.com>
2021-01-26 00:27:18 -0800

    bug fixed

--- exercise-1.py
+++ exercise-1.py
@@ -24,14 +24,14 @@ Exercise 1
 # That must mean
 
 # PART 2: State Assumptions
-#
+# for loop will stop at each index
 # TODO: State your assumptions here or say them out loud to your partner ...
 # Make sure to be SPECIFIC about what each of your assumptions is!
 
 def find_largest_diff(list_of_nums):
     """Find the largest difference between *consecutive* numbers in a list."""
     largest_diff = 0
-    for i in range(len(list_of_nums)):
+    for i in range(len(list_of_nums)-1):
         diff = abs(list_of_nums[i] - list_of_nums[i+1])
         if diff > largest_diff:
             largest_diff = diff
